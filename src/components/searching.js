572ca887b6dc84e467ea14171e5b88fbb69305df
Никита Зивенко <flyingfry@yandex.ru>
2026-01-14 13:14:41 +0300

    Delete comments

--- src/components/searching.js
+++ src/components/searching.js
@@ -2,7 +2,6 @@ import {rules, createComparison} from "../lib/compare.js";
 
 
 export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
     const compare = createComparison(
         ['skipEmptyTargetValues'],
         [rules.searchMultipleFields(
@@ -13,7 +12,6 @@ export function initSearching(searchField) {
     );
 
     return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
         return data.filter(row => compare(row, state));
     }
 }
